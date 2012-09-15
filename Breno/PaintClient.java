dfe190b364c3b4d89854ca77a2b5ff9c92bbfd40
Breno Naodi Kusunoki <brenokusunoki@gmail.com>
2012-09-15 11:23:05 -0300

    Interface Inicial Concluída

--- Breno/PaintClient.java
+++ Breno/PaintClient.java
@@ -1,54 +1,66 @@
-import java.rmi.Naming; 
-import java.rmi.RemoteException; 
-import java.net.MalformedURLException; 
-import java.rmi.NotBoundException; 
-import java.rmi.RMISecurityManager;
-import java.net.*;
-import java.io.*;
-import java.util.*;
-import java.awt.BorderLayout;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JFrame;
-import classes.*;
+/*	
+	Instituição: Universidade Estadual de Londrina
+	Disciplina: Sistemas Operacionais
+	Professor: Fábio Sakuray
+	Bimestre: 3º Bimestre
+	Ano: 2012
+	
+	Autores:
+		Breno Naodi Kusunoki
+		Luiz Guilherme Castilho Martins
+	
+	Tema:
+		Implementar um Paint Compartilhado
+	
+	Descrição:
+			O trabalho consiste em desenvolver uma aplicação em Java
+		onde diversos usuários poderão utilizar o mesmo quadro branco
+		para desenhar linhas, sendo que cada usuário terá a sua linha
+		com uma cor diferenciada dos demais.
+			Casa usuário poderá utilizar um quadro branco já criado,
+		ou criar um novo para que ele possa desenhar, assim disponibi-
+		lizando o mesmo para os demais usuários desenharem.
+*/
 
+import java.rmi.Naming; 				/* #TODO: Ver documentação do JAVA */
+import java.rmi.RemoteException; 		/* #TODO: Ver documentação do JAVA */
+import java.net.MalformedURLException; 	/* #TODO: Ver documentação do JAVA */
+import java.rmi.NotBoundException; 		/* #TODO: Ver documentação do JAVA */
+import java.rmi.RMISecurityManager;		/* #TODO: Ver documentação do JAVA */
+import java.net.*;						/* #TODO: Ver documentação do JAVA */
+import java.io.*;						/* #TODO: Ver documentação do JAVA */
+import java.util.*;						/* #TODO: Ver documentação do JAVA */
+import java.awt.BorderLayout;			/* #TODO: Ver documentação do JAVA */
+import javax.swing.JFrame;				/* #TODO: Ver documentação do JAVA */
+import javax.swing.JLabel;				/* #TODO: Ver documentação do JAVA */
+import javax.swing.JFrame;				/* #TODO: Ver documentação do JAVA */
+import java.awt.Toolkit;				/* #TODO: Ver documentação do JAVA */
+import classes.*;						/* Importa todas as classes localizadas na pasta classes */
+
+/*
+	Classe:
+		PaintClient
+
+	Descrição:
+		Fornecer um GUI para o usuário final, para que ele possa
+		criar ou desenhar linhas em um quadro branco já existente.
+*/
 
 public class PaintClient extends JFrame{
+	
+	/* Construtor da classe */
 	public PaintClient()
-    {
-        setSize(500, 500);
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new BorderLayout());
-        add(new LinePanel(), BorderLayout.CENTER); //Aqui adicionamos o frame no painel.
-        add(new JLabel("Use o mouse para desenhar"), BorderLayout.NORTH);        
+	{
+        setTitle("Paint Distribuído - Versão 1.0");    /* Define o Título da Janela que contêm o JFrame / JPanel */
+        setSize(643, 388);    /* Define o tamanho do JFrame */
+        setLocation(((Toolkit.getDefaultToolkit().getScreenSize().width  / 2) - (getWidth() / 2)),
+                    ((Toolkit.getDefaultToolkit().getScreenSize().height / 2) - (getHeight() / 2)));    /* Inicializa o frame no centro da Tela */
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    /* Atribui uma Função do Botão fechar Janela */
+        add(new LinePanel(), BorderLayout.CENTER);    /* Instancia um Painel e o adiciona no JFrame */
     }
 
-	public static void main(String[] args) throws Exception { 
-
-		System.setSecurityManager(new RMISecurityManager());
-
-		/* procura pelo objeto remoto. 
-		 * localhost eh a maquina onde esta o rmiregistry
-		 * OlaMundoService eh o nome que foi dado ao objeto remoto */
-		PaintInterface c;
-		c = (PaintInterface) Naming.lookup ("rmi://localhost/PaintService");
-		BufferedReader stdIn;
-		stdIn = new BufferedReader(new InputStreamReader(System.in));
-		String nome = "";
-		while (nome.equals ("")){
-			System.out.print ("Entre com seu nome: ");
-			nome = stdIn.readLine();
-		}
-		while (true){
-			System.out.print ("Mensagem: ");
-			String msg = stdIn.readLine ();
-			if (msg.equals ("") == false){
-				/* chamada de metodo ao objeto remoto */
-				if(c.getArray (nome, msg).equals("bulhufas") == true){
-				new PaintClient().setVisible(true);
-				}
-			}
-		}
+    /* Onde a brincadeira começa :) */
+	public static void main(String[] args) throws Exception {
+		new PaintClient().setVisible(true);    /* Instancia o objeto chamando o construtor */
 	}
 }
--- Breno/classes/LinePanel.java
+++ Breno/classes/LinePanel.java
@@ -1,68 +1,124 @@
+/*  
+    Instituição: Universidade Estadual de Londrina
+    Disciplina: Sistemas Operacionais
+    Professor: Fábio Sakuray
+    Bimestre: 3º Bimestre
+    Ano: 2012
+    
+    Autores:
+        Breno Naodi Kusunoki
+        Luiz Guilherme Castilho Martins
+    
+    Tema:
+        Implementar um Paint Compartilhado
+    
+    Descrição:
+            O trabalho consiste em desenvolver uma aplicação em Java
+        onde diversos usuários poderão utilizar o mesmo quadro branco
+        para desenhar linhas, sendo que cada usuário terá a sua linha
+        com uma cor diferenciada dos demais.
+            Casa usuário poderá utilizar um quadro branco já criado,
+        ou criar um novo para que ele possa desenhar, assim disponibi-
+        lizando o mesmo para os demais usuários desenharem.
+*/
+
 package classes;
 
-import java.awt.BasicStroke;
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Point;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.util.ArrayList;
-import java.util.List;
-
+import java.awt.BasicStroke;        /* #TODO: Ver documentação do JAVA */
+import java.awt.Color;              /* #TODO: Ver documentação do JAVA */
+import java.awt.Graphics;           /* #TODO: Ver documentação do JAVA */
+import java.awt.Graphics2D;         /* #TODO: Ver documentação do JAVA */
+import java.awt.Point;              /* #TODO: Ver documentação do JAVA */
+import java.awt.event.MouseAdapter; /* #TODO: Ver documentação do JAVA */
+import java.awt.event.MouseEvent;   /* #TODO: Ver documentação do JAVA */
+import java.util.ArrayList;         /* #TODO: Ver documentação do JAVA */
+import java.util.List;              /* #TODO: Ver documentação do JAVA */
+import javax.swing.JPanel;          /* #TODO: Ver documentação do JAVA */
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.Toolkit;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JPanel;
+/*
+    Classe:
+        LinePanel
+
+    Descrição:
+        Criará um JPanel onde será mostrado as linhas desenhadas pelo(s)
+        usuário(s).
+*/
 
 public class LinePanel extends JPanel
 {
-    List<Point> pointList = new ArrayList<Point>();
-
+    /* Construtor */
     public LinePanel()
     {
+        setLayout(null);
+        setSize(643, 388);
+        setVisible(true);
+        setBackground(Color.white);
         
-        addMouseListener(new MouseAdapter()
-        {
-            @Override
-            public void mouseClicked(MouseEvent e)
-            {
-                //Após cada clique, guardamos a posição do ponto onde foi clicado.
-                if (e.getClickCount() == 1)
-                {
-                    pointList.add(new Point(e.getX(), e.getY()));
-                    repaint(); //E pedimos para o painel se repintar.
-                }
-            }
-        });
+        
+        JButton _btnCriar = new JButton("Criar");
+        JButton _btnEntrar = new JButton("Entrar");
+        _btnCriar.setFont(new Font("Arial", Font.BOLD, 20));
+        _btnEntrar.setFont(new Font("Arial", Font.BOLD, 20));
+        _btnCriar.setBounds(130, 144, 136, 72);
+        _btnEntrar.setBounds(363, 144, 136, 72);
+        
+        add(_btnCriar);
+        add(_btnEntrar);
+
+        // addMouseListener(new MouseAdapter()
+        // {
+        //     @Override
+        //     public void mouseClicked(MouseEvent e)
+        //     {
+        //         //Após cada clique, guardamos a posição do ponto onde foi clicado.
+        //         if (e.getClickCount() == 1)
+        //         {
+        //             pointList.add(new Point(e.getX(), e.getY()));
+        //             repaint(); //E pedimos para o painel se repintar.
+        //         }
+        //     }
+        // });
     };
 
+    public void paintComponent(Graphics g) {        
+        super.paintComponent(g);            
+        Image imagem = new ImageIcon(this.getClass().getResource("/classes/Fundo_Paint.jpg")).getImage();  
+        g.drawImage(imagem, 0, 0, this);  
+    }
     /**
      * Esse método será chamado toda vez que o componente precisar ser repintado.
      * Isso é, quando a janela for parcial ou totalmente escondida e reexibida ou 
      * quando o paint for chamado.
      */
-    @Override
-    protected void paintComponent(Graphics g)
-    {
-        super.paintComponent(g);
+    // @Override
+    // protected void paintComponent(Graphics g)
+    // {
+    //     super.paintComponent(g);
         
-        //Não podemos alterar o estado do objeto g, portanto fazemos uma cópia dele.
-        Graphics2D g2d = (Graphics2D) g.create();
+    //     //Não podemos alterar o estado do objeto g, portanto fazemos uma cópia dele.
+    //     Graphics2D g2d = (Graphics2D) g.create();
 
-        if (pointList.size() < 2)
-            return;
+    //     if (pointList.size() < 2)
+    //         return;
 
-        // Percorre a nossa lista, pintando de dois em dois pontos.
-        Point lastPoint = pointList.get(0);
-        for (int i = 1; i < pointList.size(); i++)
-        {
-            g2d.setColor(Color.red);
-            g2d.setStroke(new BasicStroke(5.0f));
-            g2d.drawLine(lastPoint.x, lastPoint.y, pointList.get(i).x,
-                    pointList.get(i).y);
-            lastPoint = pointList.get(i);
-        }
+    //     // Percorre a nossa lista, pintando de dois em dois pontos.
+    //     Point lastPoint = pointList.get(0);
+    //     for (int i = 1; i < pointList.size(); i++)
+    //     {
+    //         g2d.setColor(Color.red);
+    //         g2d.setStroke(new BasicStroke(5.0f));
+    //         g2d.drawLine(lastPoint.x, lastPoint.y, pointList.get(i).x,
+    //                 pointList.get(i).y);
+    //         lastPoint = pointList.get(i);
+    //     }
         
-        //E aqui destruimos a cópia do objeto g. 
-        //Não se preocupe. O desenho feito por ele não será destruído.
-        g2d.dispose();
-    }
+    //     //E aqui destruimos a cópia do objeto g. 
+    //     //Não se preocupe. O desenho feito por ele não será destruído.
+    //     g2d.dispose();
+    // }
 }
